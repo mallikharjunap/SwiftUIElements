24d0b878eebafc3cfcd21e378ec304f37c78b93e
mallikharjunaGupta <mallikharjunaGuptaP@ecssi.com>
2024-01-12 14:50:19 +0530

    Initial Commit

---
+++ SwiftUIElements/SwiftUIElementsApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIElementsApp.swift
+//  SwiftUIElements
+//
+//  Created by Apple on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        
+        }
+    }
+}
